c336f5955869609be0a36f391c69dce0ccaa5400
Marissa Conner <talk@fairgroundcreative.com>
2019-04-12 10:29:32 -0700

    Setting up Homepage to talk to Express, not quite there yet

--- src/App/pages/Homepage.js
+++ src/App/pages/Homepage.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Homepage = () => (
-  <div>
-    <h1>
-      Homepage
-    </h1>
-    <p>Lorem ipsum</p>
-  </div>
-);
+class Homepage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount(){
+    this.getList();
+  }
+
+  getList = () =>{
+    fetch('/api/getList')
+    .then( res => res.json())
+    .then( list => this.setState({ list }))
+  }
+
+  render(){
+    const { list } = this.state;
+
+    return(
+      <div>
+        <h1>
+          Homepage
+        </h1>
+        <p>Lorem ipsum</p>
+        { list.length ? (
+           <div>
+           {list.map((item) => {
+            return (
+              <div>
+                  {item}
+              </div>
+              );
+           })}
+           </div>
+          ) : (
+            <div>Yeah that didn't work.</div>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Homepage from './App/pages/Homepage.js';
 
 import './index.css';
 import App from './App.js';
