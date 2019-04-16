33026ec0e74977e5e263520fe2f0834946fb5bff
Marissa Conner <talk@fairgroundcreative.com>
2019-04-16 12:02:35 -0700

    ...except doing it correctly this time

--- server/models/stores.js
+++ server/models/stores.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = function( sequelize, DataTypes ){
-  const Unit = sequelize.define( 'Store', 
+  const Store = sequelize.define( 'Store', 
   //attributes
   {
     name: DataTypes.STRING,
     // Perhaps later add location information -- help others out by sharing prices.
   });
 
-  Store.associate( function( models ) ){
+  Store.associate = function( models ){
     models.Store.belongsTo( models.User , { as: 'store' } );
-  } 
+  }
 
   return Store;
 }
