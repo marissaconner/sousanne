a7a63ad7b452aeaf86b55762d12ba88d02928080
Marissa Conner <talk@fairgroundcreative.com>
2019-04-16 12:00:20 -0700

    Creating the stores table

---
+++ server/models/stores.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function( sequelize, DataTypes ){
+  const Unit = sequelize.define( 'Store', 
+  //attributes
+  {
+    name: DataTypes.STRING,
+    // Perhaps later add location information -- help others out by sharing prices.
+  });
+
+  Store.associate( function( models ) ){
+    models.Store.belongsTo( models.User , { as: 'store' } );
+  } 
+
+  return Store;
+}
