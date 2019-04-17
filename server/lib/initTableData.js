4c344bae56faf7ef0bf5cf51a40c731f013dd211
Marissa Conner <talk@fairgroundcreative.com>
2019-04-17 07:33:46 -0700

    Adding info to work with to InitTableData re: recipes

--- server/lib/initTableData.js
+++ server/lib/initTableData.js
@@ -4,10 +4,43 @@ module.exports = function( models ) {
 
 const recipes = ['Nachos', 'Tacos', 'Enchiladas', 'Picadillo'];
 
+const foods = [
+  'Corn Tortillas',
+  'Ground Beef',
+  'Yellow Onion',
+  'Taco Seasoning',
+  'Enchilada Sauce',
+  'Cheddar Cheese',
+  'Monterey Jack Cheese'
+]
+
+const units = [
+  'pound',
+  'item',
+  'tablespoon',
+  'cup'
+]
+
 for( var i = 0; i < recipes.length; i++ ){
 models.Recipe.findOrCreate({
     where: {
       name: recipes[i]
+    }
+  })
+}
+
+for( var i = 0; i < foods.length; i++ ){
+models.Food.findOrCreate({
+    where: {
+      name: foods[i]
+    }
+  })
+}
+
+for( var i = 0; i < units.length; i++ ){
+models.Unit.findOrCreate({
+    where: {
+      name: units[i]
     }
   })
 }
