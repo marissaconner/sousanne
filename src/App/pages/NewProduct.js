850113950dfb4adff65b934a30d7ec25bf4e18fc
Marissa Conner <talk@fairgroundcreative.com>
2019-04-18 20:32:20 -0700

    Certain fields conditionally show up

--- src/App/pages/NewProduct.js
+++ src/App/pages/NewProduct.js
@@ -4,10 +4,30 @@ class NewProduct extends Component {
    constructor(props){
     super(props);
     this.state = {
+      packaged : false,
+      multipack: false,
     }
   }
 
+  toggleMultipack(){
+    const value = !this.state.multipack
+    this.setState({ multipack: value })
+  }
+
+  setPackaging( value ) {
+    this.setState({ packaged: value })
+  }
   render() {
+
+    const bulkunits = [
+    'Pound',
+    'Ounce',
+    'Gram',
+    'Litre',
+    'Dozen',
+    'Piece'
+    ]
+
     const units = [
       'Fluid ounces',
       'Gallons',
@@ -33,22 +53,32 @@ class NewProduct extends Component {
           <legend>Product Information</legend>
 
           <div className="form-group">
-            <input type="radio" value="false" id="isnotpackaged" /><label htmlFor="isnotpackaged">Sold by the...</label>
-            <input type="radio" value="true" id="ispackaged" /> <label htmlFor="ispackaged">Packaged item</label>
+            <input type="radio" value="false" id="isnotpackaged" onChange={()=> this.setPackaging(false) } checked={!this.state.packaged} /><label htmlFor="isnotpackaged">Sold by the...</label>
+            { this.state.packaged ? "" : (
+              <select>
+                {bulkunits.map(unit => <option>{unit}</option>)}
+              </select>
+            )}
+
+            <input type="radio" value="true" id="ispackaged"  onChange={()=> this.setPackaging(true) } checked={this.state.packaged} /> <label htmlFor="ispackaged">Packaged item</label>
           </div>
         </fieldset>
 
-        <fieldset>
-          <legend>Each item contains...</legend>
+        { this.state.packaged ? 
+          ( 
+            <fieldset>
+              <legend>Each item contains...</legend>
 
-          <input type="number" />
-          <select>
-          {units.map(unit => <option>{unit}</option>)}
-          </select>
+              <input type="number" />
+              <select>
+                {units.map(unit => <option>{unit}</option>)}
+              </select>
 
-          <input type='checkbox' id='ismultipack' /> <label htmlFor='ismultipack'>Sold in a multipack of...</label>
-          <input type='number' id='multipackcount' />
-        </fieldset>
+              <input type='checkbox' id='ismultipack' onChange={()=>this.toggleMultipack()} /> <label htmlFor='ismultipack'>Sold in a multipack of...</label>
+              {this.state.multipack ? (<input type='number' id='multipackcount' value={this.state.multipackCount} />) : ""}
+            </fieldset>
+          ) : ""
+        }
 
         <span className="btn bare">Add Price</span>
 
