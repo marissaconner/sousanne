ff479e0c667b2a30e4d6382776b63d1d67842eb0
Marissa Conner <talk@fairgroundcreative.com>
2019-10-07 12:28:47 -0700

    adding foods function

--- server/lib/initTableData.js
+++ server/lib/initTableData.js
@@ -51,46 +51,45 @@ module.exports = function( models ) {
     }
   ];
 
-  function installChildIngredient( item , parentId ){
-    //We are getting an array that has grains in it  and 1 
+ 
 
-    models.food.findOrCreate({
+  function newChildFood( item, id ){
+    models.Food.findOrCreate({ 
       where: {
         name: item.name,
-        parentId: parentId
+        parentId: id
       }
-      //we made grains now with parent id 1
     })
-    .then( function( newParent ){ //that returns an object so lets say grains is id 2
-      if( item.hasOwnProperty( 'children' ) ){ //it does
-        const childArray = newParent.children;  //so we have an array w gluten free and nongluten
-        for( var i = 0; i < childArray.length; i++ ){
-          installChildIngredient( childArray[i] , newParent.id );
-        }
-      }
+    .then( function( data ){
+      return data;
     })
   };
 
-  function installIngredients( items ){
-    for( var i = 0; i < items.length; i++ ){
-      console.log( "I am looking at items" , items[i] );
-      parentItem = items[i]
-      models.Food.findOrCreate({
-        where: {
-          name: parentItem.name,
-        }
-      })
-      .then( function ( parent ){
-        console.log( "checking for children at installIngredients" , i );
-        console.log( parentItem ); 
-        if( parentItem.hasOwnProperty( 'children' ) ) {
-          //it does
-          installChildIngredient( parentItem.children , parent.id );
-        }
-      })
+  function newFood( item ){
+    console.log( "running NewFood!!\r\n\r\n\r\n");
+    models.Food.findOrCreate({ 
+      where: {
+        name: item.name,
+       }
+    })
+    .then( function( data ){
+      return data[0].dataValues.id;
+    })
+  };
+
+
+  async function addfoods( foodarray ){
+    for( var i = 0; i < foodarray.length; i++ ){
+      console.log( "Iteration starting", i );
+
+      if( foodarray[i].hasOwnProperty('children') ){        
+        console.log("children found");
+        let parentFood = await newFood( foodarray[i]); 
+      }
+
+      else { newFood(foodarray[i]); }
     }
-  }; 
-
-  installIngredients( ingredients );
-
+  }
+    
+  addfoods( ingredients );
 }
