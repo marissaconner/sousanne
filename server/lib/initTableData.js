19259e6a683351682f0d5a08f8486b0aeb274fce
Marissa Conner <talk@fairgroundcreative.com>
2019-10-07 13:25:59 -0700

    cleanup, made more complicated list to test it out

--- server/lib/initTableData.js
+++ server/lib/initTableData.js
@@ -3,102 +3,215 @@
 module.exports = function( models ) {
 
   const ingredients = [
-    {
-      name: 'Plant-Based Products',
-      children: [
-        {
-          name: 'Grains',
-          children: [
-            {
-              name: 'Gluten-Free Grains',
-              children: [
-                {
-                  name: 'Corn'
-                }
-              ]
-            },
-            {
-              name: 'Gluten-Containing Grains'
-            }
-          ]
-        },
-        
-      ]
-    },
-    {
-      name: 'Meat And Animal Products',
-      children: [
-        {
-          name: 'Seafood',
-          children: [
-            {
-              name: 'Fish',
-              children: [
-                {
-                  name: 'Salmon'
-                },
-                {
-                  name: 'Trout'
-                },
-              ]
-            }
-          ]
-        },
-        {
-          name: 'Beef'
-        }
-      ]
-    }
-  ];
-
- 
-
-  function newChildFood( item, id ){
-    models.Food.findOrCreate({ 
-      where: {
-        name: item.name,
-        parentId: id
+  {
+    name: 'Plant-Based Products',
+    children: [
+      {
+        name: 'Vegetables'
+      },
+      {
+        name: 'Fruits'
+      },
+      {
+        name: 'Grains',
+        children: [
+          {
+            name: 'Gluten Grains',
+            children: [
+              {
+                name: 'Wheat',
+                children: [
+                  {
+                    name: 'Spelt'
+                  },
+                  {
+                    name: 'Kamut'
+                  },
+                  {
+                    name: 'Farro'
+                  },
+                  {
+                    name: 'Durum',
+                    children: [
+                      {
+                        name: 'Bulgur'
+                      },
+                      {
+                        name: 'Semolina'
+                      }
+                    ]
+                  },
+                ]
+              },
+              {
+                name: 'Barley'
+              },
+              {
+                name: 'Rye'
+              },
+              {
+                name: 'Triticale'
+              },
+            ]
+          },
+          {
+            name: 'Gluten-Free Grains',
+            children: [
+              {
+                name: 'Indian Ricegrass',
+              },
+              {
+                name: 'Chinese Pearl Barley'
+              },
+              {
+                name: 'Sorghum', 
+              },
+              {
+                name: 'Quinoa',
+              },
+              {
+                name: 'Oats',
+              },
+              {
+                name: 'Buckwheat',
+              },
+              {
+                name: 'Amaranth ',
+              },
+              {
+                name: 'Teff'
+              },
+              {
+                name: 'Rice',
+                chidren: [
+                  {
+                    name: 'Wild Rice'
+                  },
+                  {
+                    name: 'Brown Rice'
+                  },
+                  {
+                    name: 'White Rice'
+                  }
+                ]
+              },
+              {
+                name: 'Millet', 
+              },
+              {
+                name: 'Corn',
+              }
+            ]
+          }
+        ]
       }
-    })
-    .then( function( data ){
-      return data;
-    })
-  };
-
- 
-
-  /*
-  "ingredients" is an array of objects with children in them like: 
-  ingredients: 
-  [
-    { 
-      name: 'meats', 
-      children: [
-        { name: 'beef' } , 
-        {
-          name: 'poultry', 
-          children: [ 
-             {name: 'chicken'},
-             {name: 'turkey'}
-          ]
-        }
-      ]
-    }
-  ]
-  */
-
-  function newChildFood( item , id ){
-    console.log("Running new child food.\r\n\r\n");
-    models.Food.findOrCreate({ 
-      where: {
-        name: item.name,
-        parentId: id
-       }
-    })
-    .then( function( data ){
-      return data[0].dataValues.id;
-    })
-  };
+    ]
+  },
+  {
+    name: 'Animal Products',
+    children: [
+      {
+        name: 'Meats',
+        children: [
+          {
+            name: 'Beef'
+          },
+          {
+            name: 'Pork'
+          },
+          {
+            name: 'Horse'
+          },
+          {
+            name: 'Venison'
+          },
+          {
+            name: 'Sheep'
+          },
+          {
+            name: 'Goat'
+          },
+          {
+            name: 'Boar'
+          },
+          {
+            name: 'Rabbit'
+          },
+          {
+            name: 'Elk'
+          },
+          {
+          name: 'Poultry',
+          children: 
+            [
+              {
+                name: 'Chicken',
+              },
+              {
+                name: 'Duck'
+              },
+              {
+                name: 'Goose'
+              },
+              {
+               name: 'Turkey'
+              },
+              {
+                name: 'Quail'
+              },
+              {
+                name: 'Squab'
+              },
+              {
+                name: 'Pheasant'
+              },
+              {
+                name: 'Dove'
+              },
+            ]
+          },
+        ]
+      },
+      {
+        name: 'Fish'
+      },
+      {
+        name: 'Dairy',
+        children: [
+          {
+            name: 'Butter'
+          },
+          {
+            name: 'Cheese'
+          },
+          {
+            name: 'Cow Milk'
+          }
+        ]
+      },
+      {
+        name: 'Eggs',
+        children: [
+          {
+            name: 'Chicken Egg'
+          },
+          {
+            name: 'Quail Egg'
+          },
+          {
+            name: 'Duck Egg'
+          }
+        ]
+      },
+      {
+        name: 'Honey'
+      },
+      {
+        name: 'Fish Roe'
+      }
+    ]
+  }
+];
 
   async function createFood( item ){
     await new Promise( r=> setTimeout(r,250));
@@ -132,8 +245,6 @@ module.exports = function( models ) {
   }
 
   async function addChildFoods( foods, id ){
-    console.log("\r\n\r\n\r\nChild Food Array")
-
     for( var i = 0; i < foods.length; i++ ){
       if( foods[i].hasOwnProperty('children')){
         const childFood = await createChildFood( foods[i] , id );
@@ -156,7 +267,6 @@ module.exports = function( models ) {
       }
     }
   }
-
 
   addfoods( ingredients );
 }
