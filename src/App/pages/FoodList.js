77c9c65704c7b85dda127884f2ca71c20e3cb373
Marissa Conner <talk@fairgroundcreative.com>
2019-10-07 17:19:10 -0700

    Search input value corresponds visually

--- src/App/pages/FoodList.js
+++ src/App/pages/FoodList.js
@@ -4,10 +4,12 @@ class FoodList extends Component {
    constructor(props){
     super(props);
     this.state = {
-      foods: []
+      foods: [],
+      searchString : ""
     }
 
     this.searchFoods = this.searchFoods.bind(this);
+    this.clearSearch = this.clearSearch.bind(this);
   }
 
   componentDidMount(){
@@ -21,12 +23,24 @@ class FoodList extends Component {
     .then( foods => this.setState({ foods }) )
   };
 
+  clearSearch = () => {
+    this.setState( {searchString: ""})
+    this.getFoods();
+  }
+
   searchFoods = e => {
-    const query = e.target.value;
-    console.log("Searching for foods", query );
-    fetch('api/search/food/' + query )
-    .then( res => res.json())
-    .then( foods => this.setState({foods}))
+    this.setState({ searchString : e.target.value });
+    const query = this.state.searchString;
+    if( e.target.value !== "" ){
+      console.log("Searching for foods", query );
+      fetch('api/search/food/' + query )
+      .then( res => res.json())
+      .then( foods => this.setState({foods}))
+    }
+    else {
+      //Show all foods bc they cleared the search.
+      this.getFoods();
+    }
   };
 
   render() {
@@ -39,8 +53,10 @@ class FoodList extends Component {
           type="text" 
           placeholder="Search..." 
           class="search"
+          value={this.state.searchString}
           onChange = {this.searchFoods}
         />
+        <span className='button transparent' onClick = {this.clearSearch} >&times;</span>
         <button>Go</button>
       </form>
 
