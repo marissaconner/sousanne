348f2e536378e77e1a55436044dab8095a755ef7
Marissa Conner <marissafromtexas@gmail.com>
2022-06-25 17:57:55 -0700

    Basic cable setup for a React app on a Node server.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+
+  const App = () => (
+      <div>
+        Henlo
+      </div>
+    )
+
+    return (
+      <App />
+    );
+  }
+}
+
+export default App;
