2f51418993ebfe5c2deeb3582b5d444d4b8e9e5d
Marissa Conner <marissafromtexas@gmail.com>
2022-06-27 15:41:13 -0700

    signup-login postgresql server with users table

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import path from 'path'
 let app: Express.Application | undefined = undefined;
 
 app = Express()
-// app.use(Express.urlencoded({extended: true}))
+app.use(Express.urlencoded({extended: true}))
 
 // Serve the static files from the React app
 app.use(Express.static(path.join(__dirname, '/../build')))
@@ -15,5 +15,30 @@ app.get('*', function (_req: Express.Request, res: Express.Response) {
 })
 const port = process.env.PORT || 5000;
 app.listen(port);
+
 console.log('App is listening on port ' + port);
+
+const { Pool, Client } = require("pg");
+
+const credentials = {
+  user: "postgres",
+  host: "localhost",
+  database: "sousanne",
+  password: "bacon",
+  port: 5432,
+};
+
+async function connectWithPool() {
+  const pool = new Pool(credentials);
+  const now = await pool.query("SELECT NOW()");
+  await pool.end();
+  return now;
+}
+
+(async () => {
+  const result = await connectWithPool();
+  console.log("Connected to Postgres at " + result.rows[0]["now"]);
+})();
+
 module.exports = app
+
