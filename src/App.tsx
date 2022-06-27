053f14209bbbf8f8dcfd32540018642cd8469859
Marissa Conner <marissafromtexas@gmail.com>
2022-06-27 13:34:50 -0700

    FE list builder - remove items from list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 function App() {
-  // interface IList {name: string}[]
   const [list, setList] = useState<{name: string}[]>([])
 
   const handleAdd = function() {
@@ -9,13 +8,31 @@ function App() {
     setList(state => [...state, newItem])
   }
 
+  const handleRemove = function(index: number) {
+    const newList = [...list]
+    newList.splice(index,1)
+    setList(newList)
+  }
+
   return (
     <div>
       <h1>The List!</h1>
       <div id="listbuilder">
         <ul>
-          {list.map((item, key) =>
-            <li key={key}>{item.name}</li>
+          {list.map((item, idx) =>
+            <li
+              key={idx}
+              id={`list-item-${idx}`}
+            >
+              <span>
+                {item.name}
+              </span>
+              <button
+                onClick={() => handleRemove(idx)}
+              >
+                &times;
+              </button>
+            </li>
           )}
         </ul>
 
