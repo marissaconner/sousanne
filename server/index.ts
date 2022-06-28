ca07d7cc774ed062b8857cea1ac709476ae43463
Marissa Conner <marissafromtexas@gmail.com>
2022-06-28 01:32:03 -0700

    signup-login Postgres database connection

--- server/index.ts
+++ server/index.ts
@@ -1,44 +1,38 @@
 import Express from 'express'
-import http from 'http'
+import bodyParser from 'body-parser'
 import path from 'path'
+import userRouter from './routers/userRouter'
+import { Pool } from 'pg';
 
-let app: Express.Application | undefined = undefined;
-
-app = Express()
-app.use(Express.urlencoded({extended: true}))
-
-// Serve the static files from the React app
-app.use(Express.static(path.join(__dirname, '/../build')))
-
-app.get('*', function (_req: Express.Request, res: Express.Response) {
-  res.sendFile(path.join(__dirname+'/../public/index.html'))
-})
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
-
-const { Pool, Client } = require("pg");
-
-const credentials = {
-  user: "postgres",
-  host: "localhost",
-  database: "sousanne",
-  password: "bacon",
+const port = process.env.PORT || 5000
+const router = Express.Router()
+const pgConfig = {
+  max: 20,
+  idleTimeoutMillis: 30000,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sousanne',
+  password: 'bacon',
   port: 5432,
 };
 
-async function connectWithPool() {
-  const pool = new Pool(credentials);
-  const now = await pool.query("SELECT NOW()");
-  await pool.end();
-  return now;
-}
+export const db = new Pool(pgConfig)
 
-(async () => {
-  const result = await connectWithPool();
-  console.log("Connected to Postgres at " + result.rows[0]["now"]);
-})();
+let app: Express.Application | undefined = undefined
+app = Express()
+app.use(Express.urlencoded({extended: true})) 
+app.use(Express.static(path.join(__dirname, '/../build')))  
 
+app.get('/', function (req: Express.Request, res: Express.Response) {
+  res.sendFile(path.join(__dirname+'/../public/index.html'))
+})
+app.get('/hello', (request, response) => {
+  response.send('Hello world!');
+});
+
+app.use('/api/user', userRouter)
+app.listen(port)
+
+console.log('App is listening on port ' + port)
 module.exports = app
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,1 @@
-declare module 'App';
+declare module 'App'
