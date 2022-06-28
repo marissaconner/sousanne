e3da789d3e719529aed6190f9eedfbb4d5650f89
Marissa Conner <marissafromtexas@gmail.com>
2022-06-28 11:04:30 -0700

    signup-login making a form w validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,51 @@ import React, { useState } from 'react'
 
 function App() {
 
+  const validateEmail = function () {
+    const regex = /^([\w.%+-]+)@([\w-]+).([\w]{2,})$/i
+    const isValid = email.match(regex) !== null
+    setValidEmail(isValid)
+    // TODO: Check that the email doesn't already exist.
+  }
+
+  const validatePassword = function () {
+    setValidPassword(password.length >= 7)
+  }
+
+  const validateForm = function () {
+    validateEmail()
+    validatePassword()
+    setValidForm(validEmail && validPassword)
+  }
+
+  const onInputEmail = function (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setEmail(e.target.value)
+  }
+
+  const onInputPassword = function (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setPassword(e.target.value)
+  }
+
+  const onSubmit = function (e: React.FormEvent) {
+    e.preventDefault()
+    setSubmittingForm(true)
+    validateForm()
+    if (validForm) {
+      console.log("Beam that shit up")
+    }
+    setSubmittingForm(false)
+  }
+
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [validEmail, setValidEmail] = useState<boolean>(false)
+  const [validPassword, setValidPassword] = useState<boolean>(false)
+  const [validForm, setValidForm] = useState<boolean>(false)
+  const [submittingForm, setSubmittingForm] = useState<boolean>(false)
+
   return (
     <div>
-      <form>
+      <form noValidate onSubmit={(e) => {onSubmit(e)}}>
         <fieldset>
           <legend>Sign Up</legend>
           <label
@@ -13,8 +55,10 @@ function App() {
             Email
           </label>
           <input
+            value={email}
             id="signup_email"
             type="email"
+            onChange={(e) => {onInputEmail(e)}}
           >
           </input>
 
@@ -24,16 +68,15 @@ function App() {
             Password
           </label>
           <input
+            value={password}
             id="signup_password"
             type="password"
+            onChange={(e) => {onInputPassword(e)}}
           >
           </input>
         </fieldset>
-
+        <input type="submit" value="Sign Up"/>
       </form>
-      <button>
-        Sign Up
-      </button>
     </div>
   )
 }
