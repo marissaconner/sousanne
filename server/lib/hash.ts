77c222f136a86ea8d7278f2d0fc3f8a2ea5a573e
Marissa Conner <marissafromtexas@gmail.com>
2022-06-28 20:38:43 -0700

    User session created on login

--- server/lib/hash.ts
+++ server/lib/hash.ts
@@ -1,11 +1,11 @@
 const crypto = require('crypto')
 
 /**
- * Make a SHA256 hash with optional salt
- * @param {string} data
- * @param {string} [salt]  
- * @returns {string} with the hashed value.
- */
+* Make a SHA256 hash with optional salt
+* @param {string} data
+* @param {string} [salt]  
+* @returns {string} with the hashed value.
+*/
 export const createHash = function (data: string, salt?: string) {
   let sum = crypto.createHash('sha256')
   sum.update(data + salt)
@@ -13,14 +13,16 @@ export const createHash = function (data: string, salt?: string) {
 }
 
 /**
- * Compare two hashes 
- * @param {string} input
- * @param {string} compareTo
- * @param {string} [salt]
- */
+* Compare two hashes 
+* @param {string} input
+* @param {string} compareTo
+* @param {string} [salt]
+*/
 export const compareHash = function (input: string, compareTo: string, salt?: string) {
   const hashedAttempt = createHash(input, salt)
   return compareTo === hashedAttempt
 }
 
-export default createHash
+export const createRandom = function () {
+  return crypto.randomBytes(32).toString('hex')
+}
--- server/lib/schema.sql
+++ server/lib/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
+    session_hash TEXT DEFAULT null,
     created_at TIMESTAMP DEFAULT NOW()
 );
