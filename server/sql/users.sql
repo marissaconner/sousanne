8fd7d8e5e1143b4b47532fcd0fdcd73d6766c91e
Marissa Conner <marissafromtexas@gmail.com>
2022-06-30 01:23:34 -0700

    lists - updating the schema

--- server/sql/users.sql
+++ server/sql/users.sql
@@ -1,7 +1,20 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL NOT NULL,
+    id SERIAL PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     session_hash TEXT DEFAULT null,
     created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS shoppinglists (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    user_id INT REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS shoppinglistitems (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    count INT default 1,
+    obtained BOOLEAN default FALSE,
+    list_id INT REFERENCES shoppinglists(id),
 );
